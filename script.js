c9b9978b1429f3536484a69a623fb2443e4feeaf
Valerie <113336901+valcodess@users.noreply.github.com>
2022-10-13 14:58:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const time = 500;
-
+ 
 window.addEventListener("scroll", () => {
-    const currentScroll = window.pageYOffset;
-    if (currentScroll <= time) {
-      opacity = 1 - currentScroll / time;
-    } else {
-      opacity = 0;
-    }
-    document.querySelector('.fadetext1').style.opacity = opacity;
-  });
+  const scroll1 = window.pageYOffset;
+  if (scroll1 <= time) {
+    opacity = 0 - scroll1 / time;
+  } else {
+    opacity = 1;
+  }
+  document.querySelector(".fadetext1").style.opacity = opacity;
+  
+});
+
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
